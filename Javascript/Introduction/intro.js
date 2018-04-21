91de8958f4f57c1a96d28f655b809798cd5dd1e6
Sainath Ramanathan <amidstgeeks@gmail.com>
2018-04-21 13:39:56 +0530

    del

--- Javascript/Introduction/intro.js
+++
@@ -1,8 +0,0 @@
-var firstName = prompt("Type your First Name");
-var lastName = prompt("Type your Last Name");
-var age = prompt("Type your age");
-var days = age*365.25;
-
-console.log("Your full name is "+firstName+" "+lastName);
-console.log("Your age is "+age);
-alert("You were living for "+days+" days");
