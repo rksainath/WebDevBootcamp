ad55bad165821ca6c503ff8cc1486cdacbc44757
Sainath Ramanathan <amidstgeeks@gmail.com>
2018-05-07 13:13:36 +0530

    while

---
+++ Javascript/Introduction/WhileLoops/while.js
@@ -0,0 +1,30 @@
+/*console.log("Number -10 to 19");
+var number = -10;
+while(number<20){
+	console.log(number);
+	number++;
+}
+*/
+/*console.log("Number 10 to 40");
+var evenNumber = 10;
+while(evenNumber<40){
+	console.log(evenNumber);
+	evenNumber+=2;
+}*/
+
+console.log("Number 300 to 333");
+var oddNumber = 300;
+while(oddNumber<333){
+	console.log(oddNumber);
+	oddNumber+=1;
+}
+
+/*console.log("Number 5 to 50");
+var startingNumber = 5;
+while(startingNumber<50){
+	if(startingNumber%5===0 && startingNumber%3===0)
+	{
+	console.log(startingNumber);	
+}	
+	startingNumber++;
+}*/
